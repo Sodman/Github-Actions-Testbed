148a61875adbc1074c998e661ac556f3abf3cbb5
Shane O'Donnell <shane.odonnell@solo.io>
2021-09-07 12:07:31 -0400

    Use generic refs

--- ci/check-changelog-for-skip-ci.sh
+++ ci/check-changelog-for-skip-ci.sh
@@ -9,7 +9,7 @@ echo "SKIP_CI=false" >> $GITHUB_ENV
 
 if [[ $(git diff main | grep "+++ b/changelog/" | wc -l) = "       1" ]]; then
     echo "exactly one changelog added since main"
-    gitFileChange=$(git diff main | grep "+++ b/changelog/")
+    gitFileChange=$(git diff refs/heads/main..$GITHUB_REF | grep "+++ b/changelog/")
     changelogFileName=$(echo $gitFileChange | sed 's/+++ b\///g')
     echo "changelog file name == $changelogFileName"
     if [[ $(cat $changelogFileName | grep "skipCI: true") ]]; then
